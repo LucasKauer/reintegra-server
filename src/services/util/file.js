8ca8fd2c9a7070cfacc357c37d32489eba2bd700
Lucas de Freitas Kauer <lucas.kauerrs@hotmail.com>
2017-11-16 22:47:48 -0200

    style(APP): sintax sugar

--- src/services/util/file.js
+++ src/services/util/file.js
@@ -2,13 +2,13 @@ import fs from 'fs';
 import path from 'path';
 
 const readFileUploaded = (filename) => {
-    const fullPath = path.join('uploadedFiles', filename);
-    try {
-        const result = fs.createReadStream(fullPath);
-        return Promise.resolve(result);
-    } catch (err) {
-        return Promise.reject(err);
-    }
+  const fullPath = path.join('uploadedFiles', filename);
+  try {
+    const result = fs.createReadStream(fullPath);
+    return Promise.resolve(result);
+  } catch (err) {
+    return Promise.reject(err);
+  }
 };
 
 export default readFileUploaded;
