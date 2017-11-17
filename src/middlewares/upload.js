655165cbe08c5c056e134fff3de37acabcda0ac9
Lucas de Freitas Kauer <lucas.kauerrs@hotmail.com>
2017-11-16 22:48:08 -0200

    style(UPLOAD): sintax sugar

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,17 +1,17 @@
 import multer from 'multer';
 
-const documentFilter = function (req, file, cb) {
-    if (!file.originalname.match(/\.(doc(x)?|htm(l)?|odt|pdf|ppt(x)?|txt)$/)) {
-        return cb({ name: 'FileUploadError', message: 'Somente documentos (.doc, .docx, .htm, .html, .odt, .pdf, .ppt, .pptx, .txt) são aceitos!' }, false);
-    }
-    cb(null, true);
+const documentFilter = (req, file, cb) => {
+  if (!file.originalname.match(/\.(doc(x)?|htm(l)?|odt|pdf|ppt(x)?|txt)$/)) {
+    return cb({ name: 'FileUploadError', message: 'Somente documentos (.doc, .docx, .htm, .html, .odt, .pdf, .ppt, .pptx, .txt) são aceitos!' }, false);
+  }
+  return cb(null, true);
 };
 
 const storage = multer.diskStorage({
-    destination: './uploadedFiles/',
-    filename: (_, file, callback) => {
-        callback(null, `${file.originalname}__${new Date()}`);
-    },
+  destination: './uploadedFiles/',
+  filename: (_, file, callback) => {
+    callback(null, `${file.originalname}__${new Date()}`);
+  },
 });
 
 const upload = multer({ storage, fileFilter: documentFilter });
